bee1b621a2542f8ef0d1511985e982f53b66aca5
Dennis Osagiede <56970937+Dennis247@users.noreply.github.com>
2024-01-29 21:25:48 +0100

    Added discount microservoces

--- Services/Discount/Discount.API/Program.cs
+++ Services/Discount/Discount.API/Program.cs
@@ -1,3 +1,9 @@
+using Discount.API.Repositories;
+using Discount.API.Repositories.Interfaces;
+using Discount.API.Seeding;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
+var logger = builder.Services.BuildServiceProvider().GetService<ILogger<Program>>();
+DatabaseSeeder.SeedDatabase(builder.Configuration, logger);
 
 var app = builder.Build();
 
